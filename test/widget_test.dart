063eadad30c83d7451d2ad3758052e17a51403fe
Andronick Martusheff <andronick@outlook.com>
2022-10-28 11:35:23 -0700

    This commit should fail.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -25,6 +25,7 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // This should fail.
+    expect(find.text('12'), findsOneWidget);
   });
 }
